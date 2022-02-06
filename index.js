1cb8c6f0b8ca912268a58f2bb46dd45002be1d7a
ashish khatri <macad@pop-os.localdomain>
2022-02-06 15:19:18 +0545

    made index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+function shadowizard(options){
+    let images = document.querySelectorAll('.shadowizard');
+
+    if(options.shadowType === 'hard'){
+        options.shadowType = `0px`;
+    }
+
+    else{
+        options.shadowType = "15px";
+    }
+
+    images.forEach(image => {
+        image.style.boxShadow = `10px 10px ${options.shadowType} 1px rgba(0,0,0,.12)`;
+        if(options.padding){
+            image.style.padding = "1em";
+        }
+    })
+}
+
+module.exports.shadowizard = shadowizard
